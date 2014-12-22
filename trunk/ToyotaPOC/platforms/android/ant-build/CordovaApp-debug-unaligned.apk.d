9c81481a26c33d47f5e2a08f7c40a7221b16de70
sergei.mishkarudny <sergei.mishkarudny@ONLE63301PMD7W1.oakton.com.au>
2014-12-23 02:50:47 +1100

    added files for service locations functionality

--- trunk/ToyotaPOC/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ trunk/ToyotaPOC/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\projects\Toyota\git\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\projects\Toyota\git\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\ant-build\CordovaApp.ap_ \
-C:\projects\Toyota\git\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\ant-build\classes.dex \
-C:\projects\Toyota\git\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\projects\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\projects\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\ant-build\CordovaApp.ap_ \
+C:\projects\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\ant-build\classes.dex \
+C:\projects\PhoneGapLogBook\trunk\ToyotaPOC\platforms\android\CordovaLib\ant-build\classes.jar \
