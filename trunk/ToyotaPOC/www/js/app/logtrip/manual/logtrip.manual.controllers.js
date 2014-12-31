06c1a26a2d32a1d629ea2be10ad08f72f695ba33
yann.randrianantenai <yann.randrianantenai@ONLE63307VBZTY1.oakton.com.au>
2014-12-31 13:17:17 +1100

    adding SQLLite service, modify controllers and adding views for vehicles, drivers and trips

--- trunk/ToyotaPOC/www/js/app/logtrip/manual/logtrip.manual.controllers.js
+++ trunk/ToyotaPOC/www/js/app/logtrip/manual/logtrip.manual.controllers.js
@@ -1,5 +1,36 @@
-﻿var logManualControllers = angular.module('logtrip.manual.controllers', []);
+﻿var logManualControllers = angular.module('logtrip.manual.controllers', ['SQLservices']);
 
-logManualControllers.controller('LogManualController', ['$scope', function ($scope) {
-	// TODO
+logManualControllers.controller('LogManualController', ['$q', '$scope', 'Trips', 'Vehicles', 'Drivers', 'NatureOfTrip', 
+  	function($q, $scope, Trips, Vehicles, Drivers, NatureOfTrip) {
+
+	$scope.loadVehicles = function() {
+		Vehicles.all().then(function(allvehicles){
+			$scope.allvehicles = allvehicles;
+		}); 
+	}
+	
+	$scope.loadDrivers = function() {
+		Drivers.all().then(function(alldrivers){
+			$scope.alldrivers = alldrivers;
+		}); 
+	}
+
+	$scope.loadNatureOfTrips = function() {
+		NatureOfTrip.all().then(function(allnatureoftrips){
+			$scope.allnatureoftrips = allnatureoftrips;
+		}); 
+	}
+
+	$scope.addTrip = function(kmfrom, kmto, vehicleid, natureoftripid, driverid, datestart, dateend, locationfrom, locationto) {
+		Trips.add(kmfrom, kmto, vehicleid, natureoftripid, driverid, datestart, dateend, locationfrom, locationto);
+		$scope.loadVehicles();
+		$scope.loadDrivers();
+		$scope.loadNatureOfTrips();
+	}
+
+	$scope.loadVehicles();
+	$scope.loadDrivers();
+	$scope.loadNatureOfTrips();
+	
 }]);
+
