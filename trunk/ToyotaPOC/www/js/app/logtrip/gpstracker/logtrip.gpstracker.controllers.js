41eb571122c8ea82ed022bbdab3ae20b672f33a0
aiden-zadeh <zadeh.aiden@gmail.com>
2015-01-04 17:00:36 +1100

    A few styling for maps

--- trunk/ToyotaPOC/www/js/app/logtrip/gpstracker/logtrip.gpstracker.controllers.js
+++ trunk/ToyotaPOC/www/js/app/logtrip/gpstracker/logtrip.gpstracker.controllers.js
@@ -2,7 +2,8 @@
 
 gpsTrackerControllers.controller('GPSTrackerController', ['$scope', 'AppSettings', 'uiGmapGoogleMapApi', 'GeolocationService', '$log', function ($scope, AppSettings, uiGmapGoogleMapApi, GeolocationService, $log) {
     var controller = this;
-    
+
+    // Map Startup
     uiGmapGoogleMapApi.then(function (maps) {
         GeolocationService.getCurrentPosition(setupMap);
     });
@@ -12,12 +13,10 @@ gpsTrackerControllers.controller('GPSTrackerController', ['$scope', 'AppSettings
         $scope.map = { tripInProgress: false, center: { latitude: position.coords.latitude, longitude: position.coords.longitude }, zoom: AppSettings.map_zoom };
 
         $scope.startMarker = createMarker(position);
+        $log.debug(JSON.stringify(position));
     }
     
-    function createMarker(position) {
-        return { coords: { latitude: position.coords.latitude, longitude: position.coords.longitude }, options: {} };
-    }
-    
+    // UI Action: Start Trip
     $scope.startTrip = function () {
         GeolocationService.getCurrentPosition(function (position) {
             $log.debug(JSON.stringify(position));   
@@ -37,12 +36,55 @@ gpsTrackerControllers.controller('GPSTrackerController', ['$scope', 'AppSettings
         });
     };
 
+    function createMarker(position) {
+        return { coords: { latitude: position.coords.latitude, longitude: position.coords.longitude }, options: {} };
+    }
+
+    // UI Action: End Trip
     $scope.endTrip = function () {
         var trackingData = GeolocationService.clearWatch();
         $scope.map.tripInProgress = false;
+
+        $log.debug('total km:' + gps_total_km(trackingData));
         // TODO: Save Tip
         // TODO: Move to Edit Trip Page
     };
+    
+    // calculates total trip in KM
+    function gps_total_km(trackingData) {
+        var total_km = 0;
+        if (trackingData.length <= 1) return 0;
+
+        for (var i = 0; i < trackingData.length; i++) {
+
+            if (i == (trackingData.length - 1)) {
+                break;
+            }
+
+            total_km += gps_distance(trackingData[i].coords.latitude, trackingData[i].coords.longitude, trackingData[i + 1].coords.latitude, trackingData[i + 1].coords.longitude);
+        }
+
+        var total_km_rounded = total_km.toFixed(2);
+
+        return total_km_rounded;
+    }
+
+    // calculates distance in KM
+    function gps_distance(lat1, lon1, lat2, lon2) {
+        // http://www.movable-type.co.uk/scripts/latlong.html
+        var R = 6371; // km
+        var dLat = (lat2 - lat1) * (Math.PI / 180);
+        var dLon = (lon2 - lon1) * (Math.PI / 180);
+        var lat1 = lat1 * (Math.PI / 180);
+        var lat2 = lat2 * (Math.PI / 180);
+
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+
+        return d;
+    }
 
     // test 
     $scope.simulateTracking = function(){
