e603cbf80c9c66034c972685d729953f57e4ff2d
sergei.mishkarudny <sergei.mishkarudny@ONLE63301PMD7W1.oakton.com.au>
2015-01-09 11:24:19 +1100

    current position funct updated for angular routing

--- trunk/ToyotaPOC/www/js/app/locator/locator.controllers.js
+++ trunk/ToyotaPOC/www/js/app/locator/locator.controllers.js
@@ -138,8 +138,6 @@ function loadFavs()
       	
         return ;
       }
-
-      
       
       
 });  // end of controller
@@ -151,6 +149,118 @@ function loadFavs()
 
 
 locatorControllers.controller('LookupController', function($scope) 
-		{
-		   
-	    });
+{
+	var MAX_POSITIONS = 50;
+	$scope.getCurrentPosition = function() {
+		navigator.geolocation.getCurrentPosition(success2, error2, { timeout: 60000, maximumAge:60000, enableHighAccuracy: true });
+	  }
+	
+	
+	function error2(error){
+		alert(error.code + " " + error.message);
+    }
+
+
+    function success2(result)
+    {
+      var coords = result.coords;	
+      printCurrentPosition(coords);
+      mapCurrentPosition(coords);
+    }
+	
+	
+    
+    
+
+    function printCurrentPosition(coords){
+    	
+    	var latitude = coords.latitude;
+        var longitude = coords.longitude;
+    	var myLatLng = new google.maps.LatLng(latitude, longitude); 
+    	var geocoder = new google.maps.Geocoder();
+    	   
+        geocoder.geocode( { 'latLng': myLatLng}, function(results, status) {        
+            if (status == google.maps.GeocoderStatus.OK) 
+            {       	
+            	$scope.latitude = latitude;
+            	$scope.longitude = longitude;
+            	$scope.address = results[1].formatted_address;
+            }
+        
+        }); 	
+    }
+
+
+    
+    
+    function mapCurrentPosition(coords)
+    {
+    	var latitude = coords.latitude;
+        var longitude = coords.longitude;
+        var canvas = document.getElementById('lookupCanvas');	
+    	// Set the initial Lat and Long of the Google Map
+    	var myLatLng = new google.maps.LatLng(latitude, longitude);   
+        
+        
+     // Google Map options
+    	var myOptions = {
+         zoom: 15,
+         center: myLatLng,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+       
+    	var map = new google.maps.Map(canvas, myOptions);
+    	var marker = new google.maps.Marker({
+    		position: myLatLng,
+    	    map: map,
+    	    title: 'Current position'});
+    }
+
+    
+    
+    
+     $scope.saveCurrentPosition = function()
+     {
+    	  var latitude = $scope.latitude;
+    	  var long = $scope.longitude;
+    	  var address = $scope.address;
+    	 	
+    	  alert('saving '+ latitude + long + address);
+    	 	  var positions = getPositions();
+    	 	  if (positions == null)
+    	 		  positions = [];
+    	 	  
+    	 	  positions.unshift(new ServicePosition(latitude,long, address, new Date()));
+    	 	 
+    	 	  if(positions.length >= MAX_POSITIONS)
+    	 		  positions.slice();
+    	 	  
+    	 	  var _db = window.localStorage;
+    	 	  _db.setItem('positions', JSON.stringify(positions));
+    	 	  alert('saved' +  JSON.stringify(positions));    	 
+     }
+    
+     
+     
+
+     function getPositions()
+     {
+     	var _db = window.localStorage;
+     	var positions = JSON.parse(_db.getItem('positions'));
+     	
+     	  if (positions == null)
+     		    positions = [];
+     		 
+     		  return positions;
+      }
+
+     function ServicePosition(latitude,long, address, datetime)
+     {
+     	  this.latitude = latitude;
+     	  this.address = address;
+     	  this.long = long;
+     	  this.datetime = datetime;
+     }	
+	
+});// controller end
